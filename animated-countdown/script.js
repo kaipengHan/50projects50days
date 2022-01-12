550463b599679057306b0c1283bb5fbc6f6df43e
hkp <sxhkp163@163.com>
2022-01-12 20:19:30 +0800

    animated-countdown

--- animated-countdown/script.js
+++ animated-countdown/script.js
@@ -19,14 +19,14 @@ function resetDOM() {
 function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1
-
+    // 监听动画结束事件
     num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
+      if (e.animationName === 'goIn' && idx !== nextToLast) { // 当前数字进场动画结束 并且当前数字不是最后一个数字
         num.classList.remove('in')
         num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) { // 当前数字出场动画结束 并且当前数字还有兄弟数字
+        num.nextElementSibling.classList.add('in') // 兄弟数字添加进场动画
+      } else { // 所有数字执行完动画
         counter.classList.add('hide')
         finalMessage.classList.add('show')
       }
